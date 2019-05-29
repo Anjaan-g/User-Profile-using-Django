2892a1603e5a035a5072bd3287d7ebd86b08a86c
Anjaan <anjaangaire@gmail.com>
2019-05-29 18:14:32 +0545

    modification to user profile

--- oldbook/users/models.py
+++ oldbook/users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    # image = models.ImageField( upload_to = 'image/',null = True, blank = True)
 
     def __str__(self):
         return self.username
@@ -10,6 +9,8 @@ class CustomUser(AbstractUser):
 class Profile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete = models.CASCADE)
     image = models.ImageField(default='myAvatar.png',upload_to= 'profile_pics/')
+    location = models.CharField(max_length = 50, null = True, blank = True)
+    dob = models.DateTimeField()
 
     def __str__(self):
         return f'{self.user.username.capitalize()} Profile'
